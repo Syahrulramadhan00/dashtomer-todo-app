f5e55ac0910033175a02e1803d32f6da5ffe234f
syahrulramadhan00 <sahrulramadhan684@gmail.com>
2025-04-16 19:01:00 +0700

    add deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/dashtomer-todo-app/',
   plugins: [tailwindcss(), vue(), vueDevTools()],
   resolve: {
     alias: {
